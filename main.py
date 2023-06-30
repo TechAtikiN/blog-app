6269452306cea9b17e6ca444cf673145f6162d2b
techatikin <nikitakhabya03@gmail.com>
2023-06-30 11:24:41 +0530

    feat: post route for adding a blog

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from  schemas import Blog
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+import models
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get('/blog')
 def get():
     return {'data': 'blog list'}
 
 @app.post('/blog')
-def create(request: Blog):
-    return request
+def create(request: Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title = request.title, body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
